a15539be4cb18e00b7e13fcc074606bb026324ac
oupoonpoem <poonpoemkethom007@gmail.com>
2020-05-02 01:51:44 +0700

    Create try.py

---
+++ try.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+#data=pd.read_csv('sales-cut.csv')
+#data_a=pd.DataFrame()
+#file=open('sales-cut.csv',mode='r')
+#with open('sales-cut.csv') as file:
+#print(file.read())
+#file.close()
